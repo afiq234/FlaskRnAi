61ed2cef8eca136ea8a175cce22c0327108a0deb
afiq234 <50974274+afiq234@users.noreply.github.com>
2024-03-14 13:03:50 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import AppNavigation from './src/navigation/index'
+
+export default function App() {
+    return (
+        <AppNavigation/>
+    )
+  }
+
